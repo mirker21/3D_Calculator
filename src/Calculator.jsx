e02d2434478f75b83db55dc2c5daafd4475c27e2
Mira Kershisnik <mirakershisniklikesparakeets@gmail.com>
2024-02-23 09:00:59 -0700

    added further code

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -3,81 +3,119 @@ import { useState } from "react";
 export default function Calculator() {
     const [firstNum, setFirstNum] = useState('0');
     const [currentOperator, setCurrentOperator] = useState('');
-    const [secondNum, setSecondNum] = useState();
-    const [currentMemory, setCurrentMemory] = useState();
-    const [currentResult, setCurrentResult] = useState(0);
+    const [secondNum, setSecondNum] = useState('');
+    const [currentMemory, setCurrentMemory] = useState('');
+    // const [display, setDisplay] = useState(0);
 
     function handleButtonClick(event) {
+        let calculatedResult;
+
         switch (event.target.value) {
             case('MR'):
-
-                if (firstNum.length > 0 && currentOperator.length > 0 && secondNum.length > 0)
+                calculatedResult = calculate(firstNum, secondNum, currentOperator);
+                setCurrentMemory(calculatedResult);
                 break;
             
             case('M+'):
+                calculatedResult = calculate(firstNum, secondNum, currentOperator);
+                setCurrentMemory(Number(currentMemory) - Number(calculatedResult));
                 break;
             
             case('M-'):
+                calculatedResult = calculate(firstNum, secondNum, currentOperator);
+                setCurrentMemory(Number(currentMemory) - Number(calculatedResult));
                 break;
             
             case('MC'):
+                setCurrentMemory('');
                 break;
             
             case('AC'):
+                setFirstNum('0');
+                setCurrentOperator('');
+                setSecondNum('');
+                setCurrentMemory('')
                 break;
             
             case('√'):
+                setCurrentOperator('√');
+                calculatedResult = calculate(firstNum, secondNum, '√');
+                setFirstNum(calculatedResult);
                 break;
             
             case('CE'):
+                setFirstNum('');
+                setCurrentOperator('');
+                setSecondNum('');
                 break;
             
-            case('&times;'):
+            case('×'):
+                setCurrentOperator('×');
+                setSecondNum('0');
                 break;
             
             case('÷'):
+                setCurrentOperator('÷');
+                setSecondNum('0');
                 break;
             
             case('-'):
+                setCurrentOperator('-');
+                setSecondNum('0');
                 break;
             
             case('+'):
+                setCurrentOperator('+');
+                setSecondNum('0');
                 break;
             
             case('='):
+                calculatedResult = calculate(firstNum, secondNum, currentOperator);
+                setFirstNum(calculatedResult);
                 break;
             
             case('.'):
+                setFirstNum(firstNum + '.');
                 break;
             
             case('0'):
+                setFirstNum(firstNum + '0');
                 break;
             
             case('1'):
+                setFirstNum(firstNum + '1');
                 break;
             
             case('2'):
+                setFirstNum(firstNum + '2');
                 break;
             
             case('3'):
+                setFirstNum(firstNum + '3');
                 break;
             
             case('4'):
+                setFirstNum(firstNum + '4');
                 break;
             
             case('5'):
+                setFirstNum(firstNum + '5');
                 break;
             
             case('6'):
+                setFirstNum(firstNum + '6');
                 break;
             
             case('7'):
+                setFirstNum(firstNum + '7');
                 break;
             
             case('8'):
+                setFirstNum(firstNum + '8');
                 break;
             
             case('9'):
+                setFirstNum(firstNum + '9');
                 break;
             
         }
@@ -102,22 +140,57 @@ export default function Calculator() {
 
     }
 
-    function Calculate(firstNum, secondNum, currentOperator) {
+    function calculate(firstNum, secondNum, currentOperator) {
         
-        if (secondNum.length === 0 && currentOperator.length === 0) {
-            setCurrentResult(firstNum)
+        if (currentOperator.length === 0 && secondNum.length === 0) {
+            setFirstNum(firstNum)
         } else if (currentOperator === '√') {
-            setCurrentResult(Math.sqrt(firstNum))
+            setFirstNum(Math.sqrt(Number(firstNum)))
+        } else if (currentOperator !== '√' && secondNum.length > 0) {
+            switch (currentOperator) {
+
+                case '+' :
+                    setFirstNum(Number(firstNum) + Number(secondNum));
+                    break;
+
+                case '-' :
+                    setFirstNum(Number(firstNum) - Number(secondNum));
+                    break;
+
+                case '×' :
+                    setFirstNum(Number(firstNum) * Number(secondNum));
+                    break;
+
+                case '÷' :
+                    setFirstNum(Number(firstNum) * Number(secondNum));
+                    break;
+
+            }
         }
     }
 
+    let display = '';
+
+    if (secondNum.length > 0) {
+        display = firstNum;
+    } else {
+        display = secondNum;
+    }
+
     //add highlighted buttons that are active
+    //clear currentOperator and secondNum
 
     return (
         <div id="calculator-container">
             <section id="display">
                 <p>{currentMemory}</p>
-                <input type="text" pattern="/\d*/" onChange={handleChange} value={firstNum} />
+                {
+                    secondNum.length > 0
+                    ?
+                    <input type="text" pattern="/\d*/" onChange={handleChange} value={secondNum} />
+                    :
+                    <input type="text" pattern="/\d*/" onChange={handleChange} value={firstNum} />
+                }
                 <p>{currentOperator}</p>
             </section>
 
@@ -141,7 +214,7 @@ export default function Calculator() {
                 <button aria-label="4">4</button>
                 <button aria-label="5">5</button>
                 <button aria-label="6">6</button>
-                <button aria-label="multiply">&times;</button>
+                <button aria-label="multiply">×</button>
                 <button aria-label="divide">÷</button>
             </section>
 
