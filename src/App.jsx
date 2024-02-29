193262c66743871211a0b0557f9f474c39372648
Mira Kershisnik <mirakershisniklikesparakeets@gmail.com>
2024-02-29 09:15:37 -0700

    added 3D jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Calculator from './Calculator'
+import Calculator from './CalculatorContainer'
 
 function App() {
   const [count, setCount] = useState(0)
